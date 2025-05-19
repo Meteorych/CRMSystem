aef113b72620cf17c29444f6688f9ab09fd468b8
Ivan Titlov <super.titlov@inbox.ru>
2025-05-19 22:54:06 +0300

    fixes of some inconsistencies

---
+++ WebApplication1/WebApplication1/Migrations/20250518113037_RemoveConstraint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Video_VideoNumber",
+                table: "Video");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Video_VideoNumber",
+                table: "Video",
+                column: "VideoNumber",
+                unique: true);
+        }
+    }
+}
